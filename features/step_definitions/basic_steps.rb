083f2a1be93abc8c41cf2b66f7102cad1defd6c0
rhon <rhoneil.gabriel@me.com>
2018-10-17 10:15:05 +0200

    added controller articles

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,9 @@
+Given("the following articles exists") do |table|
+    table.hashes.each do |article|
+      Article.create!(article)
+    end
+  end
+
+When("I visit the site") do
+    visit root_path
+  end
