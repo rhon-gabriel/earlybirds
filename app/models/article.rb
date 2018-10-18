d710e9d817a2bc3c785e1a33fe84d7534b5d0616
rupertlion <rupertlion@gmail.com>
2018-10-18 13:56:46 +0200

    Adding status to article model and migrating

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ApplicationRecord
     validates :header, presence: true
     validates :subheader, presence: true
     validates :body, presence: true
+    validates :status, presence: true
 end
