ef2f8e9327364b3811edbaafd40696514685ffc4
rupertlion <rupertlion@gmail.com>
2018-10-18 14:52:22 +0200

    Removing edit articles in routes, for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index
   namespace :admin do
-    resources :articles, only: [:edit, :index]
+    resources :articles, only: :index
   end
 end
