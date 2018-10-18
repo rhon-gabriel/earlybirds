b74f10de38bb008cbb9571fbed40fe1fe9d073f2
rupertlion <rupertlion@gmail.com>
2018-10-18 17:02:53 +0200

    Removing unrequired approvals action from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root controller: :articles, action: :index
   namespace :admin do
     root controller: :dashboard, action: :index
-    resources :articles, only: [:index, :approvals]
+    resources :articles, only: :index
   end
 end
