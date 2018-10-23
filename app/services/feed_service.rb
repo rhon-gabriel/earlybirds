d71149f107488e3fbe76fdab61ae3dd13cd959fc
rhon <rhoneil.gabriel@me.com>
2018-10-23 13:37:26 +0200

    added swedish local news

--- app/services/feed_service.rb
+++ app/services/feed_service.rb
@@ -3,11 +3,11 @@ require 'json'
 
 module FeedService
   
-  def self.get_articles(category, pagesize)
+  def self.get_articles(category, pagesize, country)
     newsapi = News.new(Rails.application.credentials.api[:api_key])
     top_headlines = newsapi.get_top_headlines(category: category,
                                               pageSize: pagesize,
-                                              country: 'us',
-                                              language: 'en')                    
+                                              country: country,
+                                              language: 'en')              
     end
 end
