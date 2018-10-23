97b79ed76624430865bed3af681bcfcf228ed420
rhon <rhoneil.gabriel@me.com>
2018-10-23 08:58:01 +0200

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'api_news/index'
   root controller: :articles, action: :index
+    resources :api_news, only: :index
 
   namespace :admin do
     root controller: :dashboard, action: :index
-    resources :articles, only: %i[new create index update third_party]
+    resources :articles, only: %i[new create index update]
   end
 end
