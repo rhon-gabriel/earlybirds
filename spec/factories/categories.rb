6bfe6a2fc1d5b4c0b827511d7b7e2c30081b4798
Angelica Costantini <angelicacostantini07@gmail.com>
2018-10-23 20:17:32 +0200

    adding status attribute to categories, updating factory bot for article and category, updating categories spec with all existing categories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :category do
     name { "MyString" }
+    status { :general }
   end
 end
+
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Category, type: :model do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :name }
   end
+
+  describe 'names ' do 
+    it { is_expected.to respond_to :general! }
+    it { is_expected.to respond_to :general? }
+    it { is_expected.to respond_to :business! }
+    it { is_expected.to respond_to :business? }
+    it { is_expected.to respond_to :politics! }
+    it { is_expected.to respond_to :politics? }
+    it { is_expected.to respond_to :sports! }
+    it { is_expected.to respond_to :sports? }
+  end
 end
