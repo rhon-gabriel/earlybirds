2e6d66c41e0d759d87de9e1ba47f6486bc4a4243
Angelica Costantini <angelicacostantini07@gmail.com>
2018-10-25 17:02:35 +0200

    connecting categories to navbar to display only the relevant articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   before_action :load_categories, only: %i[index]
 
   def index
+    @categories = Category.all
     @articles = if params[:category]
                   Article.where(category_id: params[:category])
                 else
